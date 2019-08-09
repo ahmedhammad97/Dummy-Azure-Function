b18f370d27ce1243fc6e2afcad998a1e6084150c
AhmedHammad <hammad97official@gmail.com>
2019-08-10 00:25:24 +0200

    Add classes

---
+++ cs folder/cs/FunctionApp1/bla.cs
@@ -0,0 +1,16 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class bla
+    {
+        string x;
+    }
+}
