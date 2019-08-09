c0cd7abb9b96ce6de3cb10fcb30505e2249cb46d
AhmedHammad <hammad97official@gmail.com>
2019-08-09 23:02:18 +0200

    nest folders

---
+++ cs folder/cs/FunctionApp1/main.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class main.cs
+    {
+        
+    }
+}
